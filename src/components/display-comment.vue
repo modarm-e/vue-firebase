6e91da023072f70bb265a934167c0b660acd5db0
ChoiMinJun <zkxm008@naver.com>
2021-02-06 17:24:02 +0900

    read comments

--- src/components/display-comment.vue
+++ src/components/display-comment.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card>
     <v-card-title>
-      <v-text-field v-model="comment" outlined label="댓글 작성" @keypress.enter="save" hide-details></v-text-field>
+      <v-textarea v-model="comment" rows="3" outlined label="댓글 작성" append-icon="mdi-send" @click:append="save" hide-details></v-textarea>
     </v-card-title>
     <template v-for="(item, i) in items"> <!--(i)인덱스 넣어준 이유 v-divider를 사용하려면 key가 있어야함-->
       <v-list-item :key="item.id">
@@ -9,30 +9,32 @@
           <display-user :user="item.user"></display-user>
         </v-list-item-action>
         <v-list-item-content>
-          <v-list-item-subtitle v-text="item.comment"></v-list-item-subtitle>
+          <v-list-item-subtitle class="black--text comment" v-text="item.comment"></v-list-item-subtitle>
           <v-list-item-subtitle><display-time :time="item.createdAt"></display-time></v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-divider :key="i"></v-divider>
     </template>
     <v-list-item>
-      <v-btn @click="more" text color="primary" block>더보기</v-btn>
+      <v-btn v-if="lastDoc && items.length < article.commentCount" @click="more" v-intersect="onIntersect" text color="primary" block>더보기</v-btn>
     </v-list-item>
   </v-card>
 </template>
 <script>
+import { last } from 'lodash'
 import DisplayTime from '@/components/display-time'
 import DisplayUser from '@/components/display-user'
+const LIMIT = 5
 
 export default {
   components: { DisplayTime, DisplayUser },
-  props: ['docRef'],
+  props: ['article', 'docRef'],
   data () {
     return {
       comment: '',
       items: [],
       unsubscribe: null,
-      limit: 5
+      lastDoc: null
     }
   },
   computed: {
@@ -47,22 +49,42 @@ export default {
     if (this.unsubscribe) this.unsubscribe()
   },
   methods: {
-    subscribe () {
-      if (this.unsubscribe) this.unsubscribe()
-
-      this.unsubscribe = this.docRef.collection('comments').limit(this.limit).onSnapshot(sn => {
-        if (sn.empty) {
-          this.items = []
-          return
-        }
-        this.items = sn.docs.map(doc => {
+    snapshotToItmes (sn) {
+      this.lastDoc = last(sn.docs)
+      sn.docs.forEach(doc => {
+        const exists = this.items.some(item => doc.id === item.id)
+        if (!exists) {
           const item = doc.data()
           item.id = doc.id
           item.createdAt = item.createdAt.toDate()
           item.updatedAt = item.updatedAt.toDate()
-          return item
-        })
+          this.items.push(item)
+        }
       })
+      this.items.sort((before, after) => {
+        const beforeId = Number(before.id)
+        const afterId = Number(after.id)
+        return afterId - beforeId
+      })
+    },
+    subscribe () {
+      if (this.unsubscribe) this.unsubscribe()
+
+      this.unsubscribe = this.docRef.collection('comments').orderBy('createdAt', 'desc').limit(LIMIT).onSnapshot(sn => {
+        if (sn.empty) {
+          this.items = []
+          return
+        }
+        this.snapshotToItmes(sn)
+      })
+    },
+    async more () {
+      if (!this.lastDoc) throw Error('더이상 데이터가 없습니다.')
+      const sn = await this.docRef.collection('comments').orderBy('createdAt', 'desc').startAfter(this.lastDoc).limit(LIMIT).get()
+      this.snapshotToItmes(sn)
+    },
+    onIntersect (entries, observer, isIntersecting) {
+      if (isIntersecting) this.more()
     },
     async save () {
       const doc = {
@@ -82,11 +104,12 @@ export default {
       batch.set(this.docRef.collection('comments').doc(id), doc)
       await batch.commit()
       this.comment = ''
-    },
-    more () {
-      this.limit += 5
-      this.subscribe()
     }
   }
 }
 </script>
+<style scoped>
+.comment {
+  white-space: pre-wrap;
+}
+</style>
